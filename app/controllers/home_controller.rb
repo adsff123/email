543ec0a5e002077a9750ddf37c4591257f658608
adsff123 <adsff123@gmail.com>
2016-04-11 06:36:35 +0000

    balsong

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+    
+    def index
+        
+    end
+    def write
+        
+        @title = params[:title]
+        @content = params[:content]
+        
+    end
+end
